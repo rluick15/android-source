d4d1a6ba0917730649eb9b5b0dd848e1c492f91b
Richard Luick <rluick15@gmail.com>
2014-11-04 17:23:58 -0500

    Java Recursion Assignment Complete

--- milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
+++ milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -3,6 +3,8 @@ package com.bloc.recursion;
 import java.util.*;
 
 public class RecursionUtils extends Object {
+
+	static int mMaxNumber = 0;
 	/*
 	 * findMaxRecursively
 	 * Takes a list of numbers and finds the largest among them
@@ -14,7 +16,19 @@ public class RecursionUtils extends Object {
 	 * Hint: your base case may be a comparison of 2 numbers
 	 */
 	public static final int findMaxRecursively(List<Integer> numbers) {
-		// IMPLEMENT ME
-		return 0;
+		//IMPLEMENT ME
+		if(numbers.size() == 0) {
+			int maxNum = mMaxNumber;
+			mMaxNumber = 0;
+			return maxNum;
+		}
+
+		if(mMaxNumber < numbers.get(0)) {
+			mMaxNumber = numbers.get(0);
+		}
+
+		numbers.remove(0);
+
+		return findMaxRecursively(numbers);
 	}
 }
