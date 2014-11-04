cb1444f37c77308c114401b6f4f0366119e3f49d
Richard Luick <rluick15@gmail.com>
2014-11-04 10:59:22 -0500

    Java Security Assignment Redo

--- milestone_01_java/00_oop/security_packages/src/com/bloc/securitypackages/fruits/Apple.java
+++ milestone_01_java/00_oop/security_packages/src/com/bloc/securitypackages/fruits/Apple.java
@@ -6,6 +6,10 @@ import com.bloc.securitypackages.fruits.*;
 
 abstract class Apple extends Fruit {	
 
+	Apple(String name, int calories, Color color, double weight) {
+    	super(name, calories, color, weight);
+	}
+
 	abstract void bite();
 
 }
--- milestone_01_java/00_oop/security_packages/src/com/bloc/securitypackages/fruits/Macintosh.java
+++ milestone_01_java/00_oop/security_packages/src/com/bloc/securitypackages/fruits/Macintosh.java
@@ -4,7 +4,7 @@ import com.bloc.securitypackages.*;
 import com.bloc.securitypackages.colors.*;
 import com.bloc.securitypackages.fruits.*;
 
-public class Macintosh extends Fruit {
+public class Macintosh extends Apple {
 
 	public Macintosh() {
 		super(Macintosh.class.getSimpleName(), 200, new Red(), 0.14d);
