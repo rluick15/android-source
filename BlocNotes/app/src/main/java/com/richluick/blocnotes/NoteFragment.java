f96e2a4695f7ad5b33a38f246b1e164193f38612
Richard Luick <rluick15@gmail.com>
2014-11-10 15:58:01 -0500

    Android Views Assignment Complete

--- BlocNotes/app/src/main/java/com/richluick/blocnotes/NoteFragment.java
+++ BlocNotes/app/src/main/java/com/richluick/blocnotes/NoteFragment.java
@@ -1,6 +1,12 @@
 package com.richluick.blocnotes;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Rich on 11/10/2014.
@@ -8,5 +14,20 @@ import android.app.Fragment;
 public class NoteFragment extends Fragment {
     public NoteFragment() {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        RelativeLayout relativeLayout = new RelativeLayout(getActivity());
+
+        EditText editText = new EditText(getActivity());
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        relativeLayout.addView(editText, params);
+
+        return relativeLayout;
     }
 }
